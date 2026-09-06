71985fbbd50288c6500c41c1839f7258aceab953
elamate <elamate90@gmail.com>
2026-09-06 22:04:43 +0200

    .

--- modules/dendritic.nix
+++ modules/dendritic.nix
@@ -5,6 +5,15 @@
 }: {
   imports = [
     inputs.flake-file.flakeModules.dendritic
+    inputs.den.flakeModules.dendritic
   ];
-  flake-file.inputs.nixpkgs.url = lib.mkForce "github:nixos/nixpkgs/nixpkgs-unstable";
+
+  flake-file.inputs = {
+    nixpkgs.url = lib.mkForce "github:nixos/nixpkgs/nixpkgs-unstable";
+    den.url = "github:denful/den";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 }
